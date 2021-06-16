6461abced70204c3ebf161027098df6a77a8dc79
punyanishivam@ <punyanishivam@gmail.com>
2021-06-16 22:03:49 +0530

    Adding functionality to take input from input.txt

--- driver.py
+++ driver.py
@@ -5,50 +5,50 @@ from services.FindSlotsWithAgeService import FindSlotsWithAgeService
 from services.FindSlotsWithLPService import FindSlotsWithLPService
 from services.LicenseNumberWithAgeService import LicenseNumberWithAgeService
 
-file = open("output.txt", "w")
+input_file = open("input.txt")
+output_file = open("output.txt", "w")
 
-create_parking_lot_service = CreateParkingLotService()
-parking_lot, message = create_parking_lot_service.create_parking_lot(6)
-file.write(message)
-file.write("\n")
+lines = input_file.readlines()
 
-park_vehicle_service = ParkVehicleService()
+for line in lines:
+    if "Create_parking_lot" in line:
+        param1, param2 = line.split()[1], 0
+        create_parking_lot_service = CreateParkingLotService()
+        parking_lot, message = create_parking_lot_service.create_parking_lot(
+            int(param1))
+        output_file.write(message)
+        output_file.write("\n")
+    if "Park" in line:
+        param1, param2 = line.split()[1], line.split()[3]
+        park_vehicle_service = ParkVehicleService()
+        output_file.write(park_vehicle_service.park(
+            param1, int(param2), parking_lot))
+        output_file.write("\n")
+    if "Slot_numbers_for_driver_of_age" in line:
+        param1, param2 = line.split()[1], 0
+        find_slots_with_age_service = FindSlotsWithAgeService()
+        output_file.write(
+            str(find_slots_with_age_service.slot_numbers_for_driver_of_age(
+                parking_lot, int(param1))))
+        output_file.write("\n")
+    if "Slot_number_for_car_with_number" in line:
+        param1, param2 = line.split()[1], 0
+        find_slots_with_lp_service = FindSlotsWithLPService()
+        output_file.write(str(find_slots_with_lp_service.slot_number_for_car_with_number(
+            parking_lot, param1)))
+        output_file.write("\n")
+    if "Leave" in line:
+        param1, param2 = line.split()[1], 0
+        unpark_vehicle_service = UnParkVehicleService()
+        output_file.write(unpark_vehicle_service.leave(
+            parking_lot, int(param1)))
+        output_file.write("\n")
+    if "Vehicle_registration_number_for_driver_of_age" in line:
+        param1, param2 = line.split()[1], 0
+        licence_number_with_age_service = LicenseNumberWithAgeService()
+        output_file.write(str(licence_number_with_age_service.vehicle_registration_number_for_driver_of_age(
+            parking_lot, 18)))
+        output_file.write("\n")
 
-file.write(park_vehicle_service.park("KA-01-HH-1234", 21, parking_lot))
-file.write("\n")
-file.write(park_vehicle_service.park("PB-01-HH-1234", 21, parking_lot))
-file.write("\n")
-
-find_slots_with_age_service = FindSlotsWithAgeService()
-
-file.write(
-    str(find_slots_with_age_service.slot_numbers_for_driver_of_age(parking_lot, 21)))
-file.write("\n")
-file.write(
-    str(find_slots_with_age_service.slot_numbers_for_driver_of_age(parking_lot, 18)))
-file.write("\n")
-
-file.write(park_vehicle_service.park("PB-01-TG-2341", 21, parking_lot))
-file.write("\n")
-
-find_slots_with_lp_service = FindSlotsWithLPService()
-
-file.write(str(find_slots_with_lp_service.slot_number_for_car_with_number(
-    parking_lot, "PB-01-HH-1234")))
-file.write("\n")
-unpark_vehicle_service = UnParkVehicleService()
-file.write(unpark_vehicle_service.leave(parking_lot, 2))
-file.write("\n")
-file.write(park_vehicle_service.park("HR-29-TG-3098", 39, parking_lot))
-file.write("\n")
-file.write(str(find_slots_with_lp_service.slot_number_for_car_with_number(
-    parking_lot, "PB-01-HH-6789")))
-file.write("\n")
-
-licence_number_with_age_service = LicenseNumberWithAgeService()
-
-file.write(str(licence_number_with_age_service.vehicle_registration_number_for_driver_of_age(
-    parking_lot, 18)))
-file.write("\n")
-
-file.close()
+input_file.close()
+output_file.close()
